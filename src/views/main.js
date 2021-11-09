2d2f3868281679c780d483d8dabb4458097aaba7
Flynn Antonacci <flynn.antonacci@live.com>
2021-11-08 19:08:10 -0500

    initial commit

---
+++ src/views/main.js
@@ -0,0 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import PersonForm from '../components/personform';
+
+const Main = props => {
+  return(
+    <div>
+      <PersonForm />
+    </div>
+  )
+}
+
+export default Main;
